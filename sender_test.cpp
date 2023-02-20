785f1e3256dce19f972dc7ad39057129532ae180
SonaliTCQ <119936733+SonaliTCQ@users.noreply.github.com>
2023-02-20 16:28:07 +0530

    Update sender_test.cpp

--- sender_test.cpp
+++ sender_test.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "sender.hpp"
+#include "sender/sender.hpp"
 #include "test/catch.hpp"
 #include <iostream>
 #include <sstream>
