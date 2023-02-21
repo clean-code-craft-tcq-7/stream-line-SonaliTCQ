231b7a947c86a5d05a1eff4105f30bd90cc45fc5
SonaliTCQ <119936733+SonaliTCQ@users.noreply.github.com>
2023-02-21 11:15:06 +0530

    Update sender.hpp

--- sender/sender.hpp
+++ sender/sender.hpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<fstream>
 
-int SOC_generate_values();
-int temp_generate_values();
+int SOC_read_values();
+int temp_read_values();
 void print(int line);
 void send_values(int value, int max_no);
